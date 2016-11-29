1c7e46290764b0aab90c1a89a2ff10a651316ff2
Ihar Yachnik <jutigor@yandex.ru>
2016-11-29 22:17:32 +0300

    Update loader according to new requirements (parse nested attributes)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+const builder = (obj) => {
+  return Object
+    .keys(obj)
+    .filter(key => !parseInt(key))
+    .reduce((p, c) => {
+      p[c] = typeof obj[c] === 'object' ? builder(obj[c]) : obj[c];
+      return p;
+    }, {});
+}
+
 module.exports = function (source) {
   const data = JSON.parse(source);
 
-  const res = Object
-    .keys(data)
-    .filter(key => !parseInt(key))
-    .reduce((p, c) => {
-      p[c] = data[c]
-      return p;
-    }, {});
+  const res = builder(data);
 
   return JSON.stringify(res);
 };
